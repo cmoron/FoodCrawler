9f869faf6f131db780e088118f32165328f4c3b6
emaulandi <e.maulandi@gmail.com>
2016-01-20 02:21:09 +0100

    20601616 Minor update for the INSERT in the joint table to match Rails project

--- dbInterface.py
+++ dbInterface.py
@@ -10,30 +10,32 @@ import MySQLdb #For reading data from the database (fetchall() method).
 import _mysql_exceptions #For catchin gmysql exception (in queries)
 
 #SET YOUR DATABASE NAME HERE
-#database_name = 
+#database_name = 'randomfood_db_development_rails'
 
 ## PLEASE ADD YOUR DB CREDENTIALS BELOW
-#db =  _mysql.connect('localhost', your user here, your user secret here, database_name)
-#dbForReadingData = MySQLdb.connect('localhost', your user here, your user secret here, database_name)
-db =  _mysql.connect()
-dbForReadingData = MySQLdb.connect()
+#db =  _mysql.connect('localhost', , , database_name)
+#dbForReadingData = MySQLdb.connect('localhost', , , database_name)
+
 cursor = dbForReadingData.cursor()
 
 ##Database fields
+id_field = "id"
 recipe_table = 'recipes'
-recipe_id = 'Recipe_Id'
-recipe_name = 'Name'
+recipe_id = 'recipe_id'
+recipe_name = 'name'
 
 ingredient_table = 'ingredients'
-ingredient_id = 'Ingredient_Id'
-ingredient_name = 'Name'
+ingredient_id = 'ingredient_id'
+ingredient_name = 'name'
+
+joint_table = 'links'
 
 #This method return the primary key of an ingredient in the ingredients table
 def getKeyIngredient(ingredient):
     dbForReadingData.commit()
     cursor = dbForReadingData.cursor()
     sql1 = "USE %s;"%(database_name)
-    sql2 = "select %s from %s where %s='%s';"%(ingredient_id,ingredient_table,ingredient_name,ingredient)
+    sql2 = "select %s from %s where %s='%s';"%(id_field,ingredient_table,ingredient_name,ingredient)
     cursor.execute(sql1)
     cursor.execute(sql2)
     results = cursor.fetchall()
@@ -55,7 +57,7 @@ def getKeyRecipe(recipe):
     dbForReadingData.commit()
     cursor = dbForReadingData.cursor()
     sql1 = "USE %s;"%(database_name)
-    sql2 = "select %s from %s where %s='%s';"%(recipe_id,recipe_table,recipe_name,recipe)
+    sql2 = "select %s from %s where %s='%s';"%(id_field,recipe_table,recipe_name,recipe)
     cursor.execute(sql1)
     cursor.execute(sql2)
     results = cursor.fetchall()
@@ -110,7 +112,8 @@ class RandomFoodDataBase:
             		db.query(u"""INSERT INTO %s(%s) VALUES ('%s');"""%(recipe_table,recipe_name,recipeName))
 			print("New recipe added to the database:")
 			nbIngredients = len(recipe.ingredients)
-			recipe_id = getKeyRecipe(recipeName)
+			recipeId = getKeyRecipe(recipeName)
+			print(recipe_id)
 			print("recipe id:"+str(recipe_id))
 			#Iteration on all ingredients to add them in the db.
 			for i in range(nbIngredients) :
@@ -129,10 +132,11 @@ class RandomFoodDataBase:
 				pass
 			    
 			    db.commit()
-			    ingredient_id = getKeyIngredient(ingredientName)
+			    ingredientId = getKeyIngredient(ingredientName)
+			    print(ingredient_id)
 			    try:
 				#Complete the junction table (matches recipe with ingredient key biatch)
-				db.query("INSERT INTO recipes_ingredients VALUES (%d,%d)"%(recipe_id,ingredient_id))
+				db.query("INSERT INTO %s(%s,%s) VALUES (%d,%d)"%(joint_table,recipe_id,ingredient_id,recipeId,ingredientId))
 			    except _mysql_exceptions.IntegrityError:
 				pass
 		else:
